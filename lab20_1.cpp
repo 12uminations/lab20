acbc108db57ce91d5c394c6a3f780936cd6da4ca
12uminations <120168442+12uminations@users.noreply.github.com>
2023-02-21 17:06:05 +0700

    Update lab20_1.cpp
    cant modify PATH

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+#include<iostream>
+using namespace std;
 
+struct Rect{
+	double x,y,w,h;
+};
 
+double overlap(Rect r1,Rect r2){
+	double x = min(r1.x+r1.w,r2.x+r2.w)-max(r1.x,r2.x);
+	double y = min(r1.y,r2.y)-max(r1.y-r1.h,r2.y-r2.h);
+	if(x < 0 or y < 0) return 0;
+	else return x*y;
 
+}
+int main(){
+	Rect R1 = {1,1,5,5};
+Rect R2 = {2,2,5,5};	
+cout << overlap(R1,R2);	
 }
